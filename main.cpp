5ff62f3ca6b4c43b9ffc9e1b17136cbd4e0f4ab5
Nguyễn Thành Nhân <52344687+nhannt2528@users.noreply.github.com>
2024-01-08 01:41:58 +0700

    update ui

--- main.cpp
+++ main.cpp
@@ -5,6 +5,9 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+//    QCursor cursor(Qt::BlankCursor);
+//    QApplication::setOverrideCursor(cursor);
+//    QApplication::changeOverrideCursor(cursor);
     Home w;
     w.show();
     return a.exec();
